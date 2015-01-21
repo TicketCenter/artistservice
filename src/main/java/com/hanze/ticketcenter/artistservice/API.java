e88e8689b0be77c36ee322bcd50b38222c3c13aa
Nils Berlijn <n_berlijn@hotmail.com>
2015-01-22 00:11:43 +0100

    API key added.

---
+++ src/main/java/com/hanze/ticketcenter/artistservice/API.java
@@ -0,0 +1,24 @@
+package com.hanze.ticketcenter.artistservice;
+
+/**
+ * The API config.
+ *
+ * @author      Nils
+ * @version     1.0
+ * @since       1.0
+ */
+public class API {
+    /**
+     * The API key.
+     */
+    private static final String API_KEY = "9zC@@~z3X9bD23:R56lz(UJGC>Ldz2wX";
+
+    /**
+     * Get the API key.
+     *
+     * @return                  An API key.
+     */
+    public String getApiKey() {
+        return API_KEY;
+    }
+}
